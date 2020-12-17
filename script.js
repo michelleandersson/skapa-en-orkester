247fb08a359b70d997cc28d6a8395fd9f5556993
Michelle Andersson <michelleandersson@live.com>
2020-12-17 17:22:46 +0100

    enable btn color switch

--- script.js
+++ script.js
@@ -6,7 +6,121 @@ const toggleAudio = sound => {
     //spelas eller pausas.
 }
 
-const toggleButtonColor = button => {
-  //Här skriver du kod för att växla knapp-färgen mellan
-  //grön och orange
+// const toggleButtonColor = button => {
+//   let saxBtn = document.getElementById('saxBtn'); 
+//   if (saxBtn.style.backgroundColor == 'orange') {
+//     saxBtn.style.backgroundColor = 'green'; 
+//   } 
+//   else {
+//     saxBtn.style.backgroundColor = 'orange'; 
+//   }; 
+//   let guitarBtn = document.getElementById('guitarBtn'); 
+//   if (guitarBtn.style.backgroundColor == 'orange') {
+//     guitarBtn.style.backgroundColor = 'green'; 
+//   } else {
+//     guitarBtn.style.backgroundColor = 'orange'; 
+//   }
+// }
+
+
+
+//MAKING THE BUTTONS CHANGE COLOR WITH A SWITCH CASE
+
+// using spread syntax to convert collection to array
+// forEach is an array method
+[...document.getElementsByTagName("button")].forEach(function(item) {
+  // adding eventListener to the elements
+  item.addEventListener('click', function() {
+    // calling the methods
+    // this.id will be the id of the clicked button
+    // there is a method in the object by same name, which will be trigger
+    obj[this.id]();
+
+  })
+})
+
+var obj = {
+  saxBtn: function() { 
+    let saxBtn = document.getElementById('saxBtn'); 
+    if (saxBtn.style.backgroundColor == 'orange') {
+      saxBtn.style.backgroundColor = 'green'; 
+    } 
+    else {
+      saxBtn.style.backgroundColor = 'orange'; 
+    };
+  },
+  guitarBtn: function() {
+    let guitarBtn = document.getElementById('guitarBtn'); 
+    if (guitarBtn.style.backgroundColor == 'orange') {
+      guitarBtn.style.backgroundColor = 'green'; 
+    } 
+    else {
+      guitarBtn.style.backgroundColor = 'orange'; 
+    };
+  },
+  pianoBtn: function() {
+    let pianoBtn = document.getElementById('pianoBtn'); 
+    if (pianoBtn.style.backgroundColor == 'orange') {
+      pianoBtn.style.backgroundColor = 'green'; 
+    } 
+    else {
+      pianoBtn.style.backgroundColor = 'orange'; 
+    };
+  },
+  trumpetBtn: function() {
+    let trumpetBtn = document.getElementById('trumpetBtn'); 
+    if (trumpetBtn.style.backgroundColor == 'orange') {
+      trumpetBtn.style.backgroundColor = 'green'; 
+    } 
+    else {
+      trumpetBtn.style.backgroundColor = 'orange'; 
+    };
+  },
+  violinBtn: function() {
+    let violinBtn = document.getElementById('violinBtn'); 
+    if (violinBtn.style.backgroundColor == 'orange') {
+      violinBtn.style.backgroundColor = 'green'; 
+    } 
+    else {
+      violinBtn.style.backgroundColor = 'orange'; 
+    };
+  },
+  banjoBtn: function() {
+    let banjoBtn = document.getElementById('banjoBtn'); 
+    if (banjoBtn.style.backgroundColor == 'orange') {
+      banjoBtn.style.backgroundColor = 'green'; 
+    } 
+    else {
+      banjoBtn.style.backgroundColor = 'orange'; 
+    };
+  },
+  drumsBtn: function() {
+    let drumsBtn = document.getElementById('drumsBtn'); 
+    if (drumsBtn.style.backgroundColor == 'orange') {
+      drumsBtn.style.backgroundColor = 'green'; 
+    } 
+    else {
+      drumsBtn.style.backgroundColor = 'orange'; 
+    };
+  }, 
+  voicesBtn: function() {
+    let voicesBtn = document.getElementById('voicesBtn'); 
+    if (voicesBtn.style.backgroundColor == 'orange') {
+      voicesBtn.style.backgroundColor = 'green'; 
+    } 
+    else {
+      voicesBtn.style.backgroundColor = 'orange'; 
+    };
+  }, 
+  bellBtn: function() {
+    let bellBtn = document.getElementById('bellBtn'); 
+    if (bellBtn.style.backgroundColor == 'orange') {
+      bellBtn.style.backgroundColor = 'green'; 
+    } 
+    else {
+      bellBtn.style.backgroundColor = 'orange'; 
+    };
+  }
 }
+
+//END CHANGING BUTTON COLORS WITH SWITCH CASE 
